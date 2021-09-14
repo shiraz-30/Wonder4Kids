b99c59703319653a02773b238004b8279511108c
Hemant Kumar Yadav <hyturing@gmail.com>
2021-09-15 01:01:55 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Body from "./components/Body/Body.js"
+import Footer from "./components/Footer/Footer.js"
+import Header from "./components/Header/Header.js"
+import Login from "./components/Login/Login.js"
+import navbar from "./components/Navbar/Navbar.js"
+
+function App() {
+  return (
+    <div>
+      <h1> Working </h1>
+    </div>
+  )
+}
+
+export default App;
