57c37e8b03c123b0d8a756693186e8550fae9c25
Rahul5430 <rahul213himani@gmail.com>
2021-10-24 21:19:34 +0530

    added footer and setup chakra-ui #5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react"
+import { ChakraProvider } from '@chakra-ui/react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import theme from './theme';
 import Body from "./components/Body/Body.js"
-import Footer from "./components/Footer/Footer.js"
 import Header from "./components/Header/Header.js"
 import Login from "./components/Login/Login.js"
 import Nvbar from "./components/Navbar/Nvbar.js"
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
-    <div>
+    <ChakraProvider theme={theme}>
       <h1> Working </h1>
       <Nvbar/>
-    </div>
+      <Footer />
+    </ChakraProvider>
   )
 }
 
