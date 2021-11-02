c59193bca230d90ee85d40bd6376d803abc3b323
Hriday Agrawal <siddhivinayak@debian-BULLSEYE-live-builder-AMD64>
2021-11-02 23:29:03 +0530

    some corrections, footer removed for now

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-//       <h1> Working </h1>
-//       <Nvbar/>
+    <>
+  {/* <ChakraProvider theme={theme}> */}
+    {/* <h1> Working </h1> */}
+  <Nvbar/>
       <Login/>
-      <Footer />
-    </ChakraProvider>
+      {/* <Footer /> */}
+  {/* </ChakraProvider> */}
+</>
   )
 }
 
